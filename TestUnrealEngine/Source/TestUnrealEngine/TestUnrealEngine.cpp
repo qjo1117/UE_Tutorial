d687fc0b7bac25e438f1d92d543871189954a6e3
Parkmuk <qjo1117@naver.com>
2022-01-31 23:45:45 +0900

    Character

--- TestUnrealEngine/Source/TestUnrealEngine/TestUnrealEngine.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestUnrealEngine.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestUnrealEngine, "TestUnrealEngine" );
